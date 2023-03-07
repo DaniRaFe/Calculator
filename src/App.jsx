4a610c7b38f3861ed14ef0556d7e5517d9b30b47
Daniela Ramon Feria <l19350318@tuxtepec.tecnm.mx>
2023-03-07 10:03:57 -0600

    Diseño de los botones

--- src/App.jsx
+++ src/App.jsx
@@ -16,25 +16,25 @@ function App() {
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >C</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >/</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >*</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >-</button> 
         </td>
       </tr>
@@ -43,25 +43,26 @@ function App() {
       <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >7</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >8</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >9</button> 
         </td>
         <td rowSpan={2}>
         <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
+          style={{height: "80px"}}
           >+</button> 
           </td>
       </tr>
@@ -70,19 +71,19 @@ function App() {
       <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >4</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >5</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >6</button> 
         </td>
       </tr>
@@ -91,25 +92,26 @@ function App() {
       <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >1</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >2</button> 
         </td>
         <td> 
           <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >3</button> 
         </td>
         <td rowSpan={2}>
         <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
+          style={{height: "80px"}}
           >=</button> 
         </td>
       </tr>
@@ -119,12 +121,13 @@ function App() {
         <button
           type='button'
           className='btn btn-primary'
+          style={{width:"175px"}}
           >0</button> 
         </td>
         <td>
         <button
           type='button'
-          className='btn btn-primary'
+          className='btn btn-primary w-75'
           >.</button> 
         </td>
       </tr>
